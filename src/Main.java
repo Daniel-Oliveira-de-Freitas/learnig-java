dc60044680c3efcab2c03b398177744e7640b1a9
Daniel-Oliveira-de-Freitas <danisnake27@gmail.com>
2023-06-26 14:56:04 -0300

    Tipos de variaveis

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        // Em Java, nós temos tipos de variaveis:
+        //String; char; byte; short; int; long; boolean; float; double;
+        // alguns exemplos
+        String hw = "Hello world! ";
+        char a = 'a'; byte b = 127;
+        int i = 20; long l = 30; boolean t = true; float f = 12.6F; double d = 13.5;
+        System.out.println(hw +"-"+ b +"-"+ i +"-"+ l +"-"+ t +"-"+ f +"-"+ d +"-"+ a);
     }
 }
